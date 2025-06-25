80a39b338aaa0d4d5b2defa940baa5946630e2e8
Franz Ludwig Kostelezky <franzludwig.kostelezky@googlemail.com>
2025-06-26 00:05:45 +0200

    Add service worker and manifest for PWA support; update theme color

--- homelab_operator/static/js/service-worker.js
+++ homelab_operator/static/js/service-worker.js
@@ -3,5 +3,5 @@ self.addEventListener('install', function(event) {
   self.skipWaiting();
 });
 self.addEventListener('fetch', function(event) {
-  // You can add caching logic here if desired
+  // Caching logic here
 });
