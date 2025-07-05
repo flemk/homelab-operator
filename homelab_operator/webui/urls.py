9a169efc72923ef4093cbf1af763663c37dbd89e
Franz Ludwig Kostelezky <franzludwig.kostelezky@googlemail.com>
2025-07-05 19:04:10 +0200

    Fix URL patterns by removing trailing slashes for 'confirm' and 'create/shutdown_url' paths

--- homelab_operator/webui/urls.py
+++ homelab_operator/webui/urls.py
@@ -13,7 +13,7 @@ urlpatterns = [
     path('logout/', views.logout_view, name='logout'),
 
     path('cron/<str:api_key>/', views.cron, name='cron'),
-    path('confirm/', views.confirm, name='confirm'),
+    path('confirm', views.confirm, name='confirm'),
     path('cron/<str:api_key>/', views.cron, name='cron'),
 
     path('wake/<int:server_id>/', views.wake, name='wake'),
@@ -48,7 +48,7 @@ urlpatterns = [
     path('edit/wiki/<int:wiki_id>/', views.edit_wiki, name='edit_wiki'),
     path('delete/wiki/<int:wiki_id>/', views.delete_wiki, name='delete_wiki'),
 
-    path('create/shutdown_url/<int:server_id>/',
+    path('create/shutdown_url/<int:server_id>',
          views.create_shutdown_url, name='create_shutdown_url'),
     path('edit/shutdown_url/<int:shutdown_url_id>/',
          views.edit_shutdown_url, name='edit_shutdown_url'),
