733af79b1f150c35bb8a3689c735621b65bc50be
Franz Ludwig Kostelezky <franzludwig.kostelezky@googlemail.com>
2025-07-07 15:44:44 +0200

    Fix default superuser password

--- homelab_operator/webui/management/commands/create_default_superuser.py
+++ homelab_operator/webui/management/commands/create_default_superuser.py
@@ -9,7 +9,7 @@ class Command(BaseCommand):
         User = get_user_model()
         username = os.getenv("SUPERUSER_USERNAME", "admin")
         email = os.getenv("SUPERUSER_EMAIL", "admin@example.com")
-        password = os.getenv("SUPERUSER_PASSWORD", "adminpass")
+        password = os.getenv("SUPERUSER_PASSWORD", "admin_password")
 
         if not User.objects.filter(username=username).exists():
             User.objects.create_superuser(username=username, email=email, password=password)
